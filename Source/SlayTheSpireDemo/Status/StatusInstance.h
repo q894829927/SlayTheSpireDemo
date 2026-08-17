0749a138144d190c3ecf45753ea922abd89cbd72
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-17 23:54:58 +0800

    feat: add phase 5A status instance

---
+++ Source/SlayTheSpireDemo/Status/StatusInstance.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "StatusInstance.generated.h"
+
+class ACombatant;
+class UStatusContainer;
+class UStatusData;
+
+UCLASS()
+class SLAYTHESPIREDEMO_API UStatusInstance : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	const UStatusData* GetDefinition() const;
+	FName GetStatusId() const;
+	int32 GetAmount() const;
+	uint64 GetRuntimeSequence() const;
+	ACombatant* GetOwner() const;
+	FString GetDebugLabel() const;
+
+private:
+	friend class UStatusContainer;
+
+	void Initialize(UStatusData* InDefinition, ACombatant* InOwner, int32 InAmount, uint64 InRuntimeSequence);
+	bool AddAmount(int32 AmountToAdd);
+
+	UPROPERTY(Transient)
+	TObjectPtr<UStatusData> Definition = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<ACombatant> Owner = nullptr;
+
+	int32 Amount = 0;
+	uint64 RuntimeSequence = 0;
+};
