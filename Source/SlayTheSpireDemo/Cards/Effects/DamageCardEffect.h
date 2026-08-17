42d846c952621e8c327be9102eabec8a3794328e
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-18 00:25:27 +0800

    feat: add explicit damage kind to card damage effects

--- Source/SlayTheSpireDemo/Cards/Effects/DamageCardEffect.h
+++ Source/SlayTheSpireDemo/Cards/Effects/DamageCardEffect.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "CardEffect.h"
+#include "../../Modifiers/ModifierTypes.h"
 #include "DamageCardEffect.generated.h"
 
 UCLASS(EditInlineNew, DefaultToInstanced)
@@ -12,6 +13,9 @@ class SLAYTHESPIREDEMO_API UDamageCardEffect : public UCardEffect
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Card|Effect", meta = (ClampMin = "0"))
 	int32 BaseAmount = 6;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Card|Effect")
+	EDamageKind DamageKind = EDamageKind::Attack;
 
 	virtual void BuildActions(
 		const FCardPlayContext& Context,
