3ebcaca27525605199b2d2bb6e6c2fe6968fbbd9
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-17 21:34:36 +0800

    feat: build damage actions from card effects

---
+++ Source/SlayTheSpireDemo/Cards/Effects/DamageCardEffect.cpp
@@ -0,0 +1,20 @@
+#include "DamageCardEffect.h"
+
+#include "../CardPlayContext.h"
+#include "../../Actions/DamageAction.h"
+
+void UDamageCardEffect::BuildActions(
+	const FCardPlayContext& Context,
+	TArray<UBattleAction*>& OutActions
+) const
+{
+	if (!IsValid(Context.ActionOuter))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[CardEffect] Damage build skipped: invalid ActionOuter."));
+		return;
+	}
+
+	UDamageAction* Action = NewObject<UDamageAction>(Context.ActionOuter);
+	Action->Initialize(Context.Source, Context.Target, BaseAmount);
+	OutActions.Add(Action);
+}
