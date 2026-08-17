0d9eef05adaee031c4f2eb4ab90a583bb55551fe
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-17 20:09:53 +0800

    refactor: pass action queue into action execution

--- Source/SlayTheSpireDemo/Actions/BattleAction.cpp
+++ Source/SlayTheSpireDemo/Actions/BattleAction.cpp
@@ -1,6 +1,6 @@
 #include "BattleAction.h"
 
-void UBattleAction::Execute()
+void UBattleAction::Execute(UBattleActionQueue* /*Queue*/)
 {
 	UE_LOG(LogTemp, Error, TEXT("[Action] Base BattleAction executed directly: %s"), *GetNameSafe(this));
 	Finish();
