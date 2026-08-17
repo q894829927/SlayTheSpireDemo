e327a8799bfde32d719feb2a03abf68f8172020d
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-17 21:35:23 +0800

    feat: add data-driven card definition asset

---
+++ Source/SlayTheSpireDemo/Cards/CardData.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CardTypes.h"
+#include "CardData.generated.h"
+
+class UCardEffect;
+
+UCLASS(BlueprintType)
+class SLAYTHESPIREDEMO_API UCardData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Card|Identity")
+	FName CardId = NAME_None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Card|Identity")
+	FText DisplayName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Card|Rules")
+	ECardType CardType = ECardType::Attack;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Card|Rules")
+	ECardTargetType TargetType = ECardTargetType::Enemy;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Card|Rules", meta = (ClampMin = "0"))
+	int32 BaseCost = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Card|Rules")
+	ECardDestination DefaultDestination = ECardDestination::Discard;
+
+	UPROPERTY(EditAnywhere, Instanced, BlueprintReadOnly, Category = "Card|Effects")
+	TArray<TObjectPtr<UCardEffect>> Effects;
+};
