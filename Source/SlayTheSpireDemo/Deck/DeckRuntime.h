4589f151188ca5828ce4a2371948f8048ca4f322
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-17 21:36:05 +0800

    feat: migrate deck runtime to card instances

--- Source/SlayTheSpireDemo/Deck/DeckRuntime.h
+++ Source/SlayTheSpireDemo/Deck/DeckRuntime.h
@@ -2,8 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
-#include "DeckTypes.h"
+#include "../Cards/CardTypes.h"
 #include "DeckRuntime.generated.h"
+
+class UCardData;
+class UCardInstance;
 
 UCLASS()
 class SLAYTHESPIREDEMO_API UDeckRuntime : public UObject
@@ -11,39 +14,53 @@ class SLAYTHESPIREDEMO_API UDeckRuntime : public UObject
 	GENERATED_BODY()
 
 public:
-	void InitializeDebugDeck(int32 Seed);
+	void InitializeFromDefinitions(const TArray<TObjectPtr<UCardData>>& Definitions, int32 Seed);
 
 	bool HasCardsInDrawPile() const;
 	bool HasCardsInDiscardPile() const;
 	bool IsHandFull() const;
+	bool IsCardInHand(const UCardInstance* Card) const;
+	bool IsCardInPlayArea(const UCardInstance* Card) const;
 
-	bool TryDrawTopCard(FDeckCardToken& OutCard);
-	bool GetFirstHandCard(FDeckCardToken& OutCard) const;
-	bool TryDiscardCardByRuntimeId(int32 RuntimeId, FDeckCardToken& OutCard);
+	bool TryDrawTopCard(UCardInstance*& OutCard);
+	UCardInstance* GetFirstHandCard() const;
+	bool TryDiscardCard(UCardInstance* Card);
+	bool TryMoveHandCardToPlayArea(UCardInstance* Card);
+	bool TryReturnPlayAreaCardToHand(UCardInstance* Card);
+	bool TryMovePlayAreaCardToDestination(UCardInstance* Card, ECardDestination Destination);
 	bool ShuffleDiscardIntoDrawPile();
 
 	int32 GetDrawCount() const;
 	int32 GetHandCount() const;
 	int32 GetDiscardCount() const;
 	int32 GetExhaustCount() const;
+	int32 GetPlayAreaCount() const;
+	int32 GetRemovedCount() const;
 
 	FString DescribeState() const;
 	void LogState(const TCHAR* Context) const;
 
 private:
 	UPROPERTY(Transient)
-	TArray<FDeckCardToken> DrawPile;
+	TArray<TObjectPtr<UCardInstance>> DrawPile;
 
 	UPROPERTY(Transient)
-	TArray<FDeckCardToken> Hand;
+	TArray<TObjectPtr<UCardInstance>> Hand;
 
 	UPROPERTY(Transient)
-	TArray<FDeckCardToken> DiscardPile;
+	TArray<TObjectPtr<UCardInstance>> DiscardPile;
 
 	UPROPERTY(Transient)
-	TArray<FDeckCardToken> ExhaustPile;
+	TArray<TObjectPtr<UCardInstance>> ExhaustPile;
+
+	UPROPERTY(Transient)
+	TArray<TObjectPtr<UCardInstance>> PlayArea;
+
+	UPROPERTY(Transient)
+	TArray<TObjectPtr<UCardInstance>> RemovedPile;
 
 	FRandomStream RandomStream;
 	int32 InitialSeed = 1337;
+	int32 NextRuntimeId = 1;
 	int32 MaxHandSize = 10;
 };
