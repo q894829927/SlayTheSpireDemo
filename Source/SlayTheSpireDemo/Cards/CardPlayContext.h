849d4d31f80707d9f7b9ae28becde63ad90725e1
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-18 14:28:38 +0800

    feat(phase6c): carry event dispatch context through card actions

--- Source/SlayTheSpireDemo/Cards/CardPlayContext.h
+++ Source/SlayTheSpireDemo/Cards/CardPlayContext.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "CoreMinimal.h"
+
 class ACombatant;
+class UBattleEventDispatcher;
 class UCardInstance;
 class UDeckRuntime;
 class UObject;
@@ -11,5 +14,7 @@ struct FCardPlayContext
 	ACombatant* Source = nullptr;
 	ACombatant* Target = nullptr;
 	UDeckRuntime* Deck = nullptr;
+	UBattleEventDispatcher* EventDispatcher = nullptr;
+	TArray<ACombatant*> EventCombatants;
 	UObject* ActionOuter = nullptr;
 };
