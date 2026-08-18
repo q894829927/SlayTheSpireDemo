c4439bec03a55769e9a1efb54b614addae220245
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-18 10:54:39 +0800

    feat: add on-demand status trigger dispatcher

---
+++ Source/SlayTheSpireDemo/Events/BattleEventDispatcher.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "BattleEventDispatcher.generated.h"
+
+class ACombatant;
+class UBattleActionQueue;
+struct FBattleEvent;
+
+struct FTriggerDispatchRecord
+{
+	FName StatusId = NAME_None;
+	int32 Priority = 0;
+	uint64 RuntimeSequence = 0;
+	int32 LocalTriggerIndex = INDEX_NONE;
+};
+
+UCLASS()
+class SLAYTHESPIREDEMO_API UBattleEventDispatcher : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	bool Dispatch(
+		const FBattleEvent& Event,
+		UBattleActionQueue* Queue,
+		const TArray<ACombatant*>& Combatants,
+		TArray<FTriggerDispatchRecord>* OutDispatchTrace = nullptr
+	) const;
+};
