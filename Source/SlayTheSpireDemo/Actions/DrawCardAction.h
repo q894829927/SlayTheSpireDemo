9a608f71a50dfc2011f5d38d3796ea8ab133128e
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-18 14:28:46 +0800

    feat(phase6c): propagate shuffle event dependencies through draw action

--- Source/SlayTheSpireDemo/Actions/DrawCardAction.h
+++ Source/SlayTheSpireDemo/Actions/DrawCardAction.h
@@ -4,6 +4,8 @@
 #include "BattleAction.h"
 #include "DrawCardAction.generated.h"
 
+class ACombatant;
+class UBattleEventDispatcher;
 class UDeckRuntime;
 
 UCLASS()
@@ -12,10 +14,20 @@ class SLAYTHESPIREDEMO_API UDrawCardAction : public UBattleAction
 	GENERATED_BODY()
 
 public:
-	void Initialize(UDeckRuntime* InDeck);
+	void Initialize(
+		UDeckRuntime* InDeck,
+		UBattleEventDispatcher* InEventDispatcher,
+		const TArray<ACombatant*>& InEventCombatants
+	);
 	virtual void Execute(UBattleActionQueue* Queue) override;
 
 private:
 	UPROPERTY(Transient)
 	TObjectPtr<UDeckRuntime> Deck = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UBattleEventDispatcher> EventDispatcher = nullptr;
+
+	UPROPERTY(Transient)
+	TArray<TObjectPtr<ACombatant>> EventCombatants;
 };
