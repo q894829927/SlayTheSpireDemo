8bdaabbf684189c2a672cef8695bb27e965180a7
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-18 14:28:31 +0800

    feat(phase6c): add typed deck shuffled event

--- Source/SlayTheSpireDemo/Events/BattleEvent.h
+++ Source/SlayTheSpireDemo/Events/BattleEvent.h
@@ -3,10 +3,23 @@
 #include "CoreMinimal.h"
 
 class ACombatant;
+class UDeckRuntime;
+
+enum class EBattleEventType : uint8
+{
+	None,
+	TurnEnded,
+	DeckShuffled
+};
 
 struct FTurnEndedEvent
 {
 	ACombatant* TurnOwner = nullptr;
+};
+
+struct FDeckShuffledEvent
+{
+	UDeckRuntime* Deck = nullptr;
 };
 
 struct FBattleEvent
@@ -14,7 +27,16 @@ struct FBattleEvent
 	static FBattleEvent MakeTurnEnded(ACombatant* TurnOwner)
 	{
 		FBattleEvent Event;
+		Event.Type = EBattleEventType::TurnEnded;
 		Event.TurnEndedPayload.TurnOwner = TurnOwner;
+		return Event;
+	}
+
+	static FBattleEvent MakeDeckShuffled(UDeckRuntime* Deck)
+	{
+		FBattleEvent Event;
+		Event.Type = EBattleEventType::DeckShuffled;
+		Event.DeckShuffledPayload.Deck = Deck;
 		return Event;
 	}
 
@@ -25,11 +47,19 @@ struct FBattleEvent
 	}
 
 private:
+	EBattleEventType Type = EBattleEventType::None;
 	FTurnEndedEvent TurnEndedPayload;
+	FDeckShuffledEvent DeckShuffledPayload;
 };
 
 template <>
 inline const FTurnEndedEvent* FBattleEvent::TryGet<FTurnEndedEvent>() const
 {
-	return &TurnEndedPayload;
+	return Type == EBattleEventType::TurnEnded ? &TurnEndedPayload : nullptr;
+}
+
+template <>
+inline const FDeckShuffledEvent* FBattleEvent::TryGet<FDeckShuffledEvent>() const
+{
+	return Type == EBattleEventType::DeckShuffled ? &DeckShuffledPayload : nullptr;
 }
