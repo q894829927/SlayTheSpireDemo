a5464162a1017648cd1a0486f4ee13e7270e1a5f
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-18 14:29:14 +0800

    feat(phase6c): give shuffle action explicit event dispatch context

--- Source/SlayTheSpireDemo/Actions/ShuffleDeckAction.h
+++ Source/SlayTheSpireDemo/Actions/ShuffleDeckAction.h
@@ -4,6 +4,8 @@
 #include "BattleAction.h"
 #include "ShuffleDeckAction.generated.h"
 
+class ACombatant;
+class UBattleEventDispatcher;
 class UDeckRuntime;
 
 UCLASS()
@@ -12,10 +14,20 @@ class SLAYTHESPIREDEMO_API UShuffleDeckAction : public UBattleAction
 	GENERATED_BODY()
 
 public:
-	void Initialize(UDeckRuntime* InDeck);
+	void Initialize(
+		UDeckRuntime* InDeck,
+		UBattleEventDispatcher* InEventDispatcher,
+		const TArray<ACombatant*>& InEventCombatants
+	);
 	virtual void Execute(UBattleActionQueue* Queue) override;
 
 private:
 	UPROPERTY(Transient)
 	TObjectPtr<UDeckRuntime> Deck = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UBattleEventDispatcher> EventDispatcher = nullptr;
+
+	UPROPERTY(Transient)
+	TArray<TObjectPtr<ACombatant>> EventCombatants;
 };
