9fd727bcac97302aee00ca03a242a4c033d78db1
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-20 01:56:16 +0800

    Add atlas presentation data to statuses

--- Source/SlayTheSpireDemo/Status/StatusData.h
+++ Source/SlayTheSpireDemo/Status/StatusData.h
@@ -7,6 +7,32 @@
 #include "../Modifiers/Damage/DamageModifier.h"
 #include "StatusData.generated.h"
 
+USTRUCT(BlueprintType)
+struct SLAYTHESPIREDEMO_API FStatusAtlasRegion
+{
+	GENERATED_BODY()
+
+	// When false the HUD should not sample the shared status atlas for this status.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Status|Presentation")
+	bool bUseAtlasIcon = false;
+
+	// Normalized atlas-space origin consumed by M_StatusAtlas.UVOffset.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Status|Presentation")
+	FVector2D UVOffset = FVector2D::ZeroVector;
+
+	// Normalized packed-region size consumed by M_StatusAtlas.UVScale.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Status|Presentation")
+	FVector2D UVScale = FVector2D::UnitVector;
+
+	// Normalized position of the trimmed image inside its original logical canvas.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Status|Presentation")
+	FVector2D TrimOffset = FVector2D::ZeroVector;
+
+	// Normalized trimmed-image size relative to the original logical canvas.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Status|Presentation")
+	FVector2D TrimScale = FVector2D::UnitVector;
+};
+
 UCLASS(BlueprintType)
 class SLAYTHESPIREDEMO_API UStatusData : public UPrimaryDataAsset
 {
@@ -18,6 +44,9 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Status|Identity")
 	FText DisplayName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Status|Presentation")
+	FStatusAtlasRegion IconRegion;
 
 	UPROPERTY(EditAnywhere, Instanced, BlueprintReadOnly, Category = "Status|Modifiers|Damage")
 	TArray<TObjectPtr<UDamageModifier>> DamageModifiers;
