218e6cf1b11cf67488e2ccf67cdae82d6f77e52a
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-21 19:09:32 +0800

    feat(ui-a2b): add damage presentation participant context

--- Source/SlayTheSpireDemo/Actions/DamageAction.h
+++ Source/SlayTheSpireDemo/Actions/DamageAction.h
@@ -14,6 +14,7 @@ class SLAYTHESPIREDEMO_API UDamageAction : public UBattleAction
 
 public:
 	void Initialize(ACombatant* InSource, ACombatant* InTarget, int32 InBaseAmount, EDamageKind InDamageKind);
+	void SetPresentationParticipantIds(FName InSourcePresentationId, FName InTargetPresentationId);
 	virtual void Execute(UBattleActionQueue* Queue) override;
 
 private:
@@ -25,4 +26,6 @@ private:
 
 	int32 BaseAmount = 0;
 	EDamageKind DamageKind = EDamageKind::Attack;
+	FName SourcePresentationId = NAME_None;
+	FName TargetPresentationId = NAME_None;
 };
