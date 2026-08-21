7b0654c50dbb566557b868a6ffdbfaa159e1d1c5
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-08-21 22:34:34 +0800

    test(ui-a2c): expose working presentation snapshot

--- Source/SlayTheSpireDemo/Presentation/BattlePresentationControllerTesting.cpp
+++ Source/SlayTheSpireDemo/Presentation/BattlePresentationControllerTesting.cpp
@@ -5,4 +5,17 @@ void UBattlePresentationController::ExpireActivePlaybackForTesting()
 {
 	HandleActiveTimeout(0.0f);
 }
+
+bool UBattlePresentationController::TryGetWorkingSnapshotForTesting(
+	FPresentationStateSnapshot& OutSnapshot
+) const
+{
+	OutSnapshot = FPresentationStateSnapshot{};
+	if (!bHasWorkingPresentationSnapshot)
+	{
+		return false;
+	}
+	OutSnapshot = WorkingPresentationSnapshot;
+	return true;
+}
 #endif
