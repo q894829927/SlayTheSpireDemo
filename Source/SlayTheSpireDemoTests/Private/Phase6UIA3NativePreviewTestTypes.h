8eae36f1b5077f02e96e575583ace2d355bcfbc5
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-09-02 13:20:49 +0800

    test(ui-a3): add native preview integration probes

---
+++ Source/SlayTheSpireDemoTests/Private/Phase6UIA3NativePreviewTestTypes.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BattleHUDCombatantPresentationWidgetBase.h"
+#include "UI/BattleHUDWidget.h"
+#include "UI/BattleHUDWidgetBase.h"
+#include "Phase6UIA3NativePreviewTestTypes.generated.h"
+
+class UBattleHUDViewModel;
+class UOverlay;
+
+UCLASS(Transient)
+class SLAYTHESPIREDEMOTESTS_API UPhase6UIA3PreviewCombatantProbe
+	: public UBattleHUDCombatantPresentationWidgetBase
+{
+	GENERATED_BODY()
+};
+
+UCLASS(Transient)
+class SLAYTHESPIREDEMOTESTS_API UPhase6UIA3PreviewHUDProbe : public UBattleHUDWidget
+{
+	GENERATED_BODY()
+
+public:
+	void ConfigurePreviewSurface(UBattleHUDViewModel* InViewModel, UOverlay* InOverlay);
+	void ReleasePreviewSurfaceForTesting();
+};
+
+UCLASS(Transient)
+class SLAYTHESPIREDEMOTESTS_API UPhase6UIA3BaseHUDProbe : public UBattleHUDWidgetBase
+{
+	GENERATED_BODY()
+};
+
+UCLASS(Transient)
+class SLAYTHESPIREDEMOTESTS_API UPhase6UIA3PreviewEventSink : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	void ObserveViewModel(UBattleHUDViewModel* InViewModel);
+
+	UFUNCTION()
+	void HandleInspectRequested(UBattleHUDCombatantPresentationWidgetBase* Presentation);
+
+	UFUNCTION()
+	void HandleInspectCleared(UBattleHUDCombatantPresentationWidgetBase* Presentation);
+
+	UFUNCTION()
+	void HandlePreviewRequested(int32 TargetId);
+
+	UFUNCTION()
+	void HandlePreviewCleared();
+
+	UFUNCTION()
+	void HandleViewModelChanged();
+
+	int32 InspectRequestedCount = 0;
+	int32 InspectClearedCount = 0;
+	int32 PreviewRequestedCount = 0;
+	int32 PreviewClearedCount = 0;
+	int32 LastPreviewTargetId = INDEX_NONE;
+	bool bObservedPreRequestPreviewClear = false;
+
+protected:
+	virtual void BeginDestroy() override;
+
+private:
+	TWeakObjectPtr<UBattleHUDViewModel> ObservedViewModel;
+};
