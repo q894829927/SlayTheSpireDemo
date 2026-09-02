b25b0ed8420dc3f994bcd76d95eca8d207e61395
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-09-02 23:52:28 +0800

    feat(phase7b): resolve status and relic trigger sources

--- Source/SlayTheSpireDemo/Events/BattleTrigger.cpp
+++ Source/SlayTheSpireDemo/Events/BattleTrigger.cpp
@@ -1,6 +1,34 @@
 #include "BattleTrigger.h"
 
+#include "../Relics/RelicInstance.h"
 #include "../Status/StatusInstance.h"
+
+FTriggerRuntimeSource FTriggerRuntimeSource::FromStatus(UStatusInstance* Instance)
+{
+	FTriggerRuntimeSource Source;
+	Source.Kind = ETriggerRuntimeSourceKind::Status;
+	Source.RuntimeObject = Instance;
+	if (IsValid(Instance))
+	{
+		Source.SourceId = Instance->GetStatusId();
+		Source.RuntimeSequence = Instance->GetRuntimeSequence();
+		Source.CombatantOwner = Instance->GetOwner();
+	}
+	return Source;
+}
+
+FTriggerRuntimeSource FTriggerRuntimeSource::FromRelic(URelicInstance* Instance)
+{
+	FTriggerRuntimeSource Source;
+	Source.Kind = ETriggerRuntimeSourceKind::Relic;
+	Source.RuntimeObject = Instance;
+	if (IsValid(Instance))
+	{
+		Source.SourceId = Instance->GetRelicId();
+		Source.RuntimeSequence = Instance->GetRuntimeSequence();
+	}
+	return Source;
+}
 
 FTriggerContext::FTriggerContext(
 	UStatusInstance* InRuntimeSource,
@@ -8,7 +36,7 @@ FTriggerContext::FTriggerContext(
 	const FPresentationRecordWriter& InPresentationRecordWriter
 )
 	: FTriggerContext(
-		InRuntimeSource,
+		FTriggerRuntimeSource::FromStatus(InRuntimeSource),
 		InActionOuter,
 		nullptr,
 		InPresentationRecordWriter
@@ -22,22 +50,80 @@ FTriggerContext::FTriggerContext(
 	ABattleManager* InBattle,
 	const FPresentationRecordWriter& InPresentationRecordWriter
 )
+	: FTriggerContext(
+		FTriggerRuntimeSource::FromStatus(InRuntimeSource),
+		InActionOuter,
+		InBattle,
+		InPresentationRecordWriter
+	)
+{
+}
+
+FTriggerContext::FTriggerContext(
+	URelicInstance* InRuntimeSource,
+	UObject* InActionOuter,
+	ABattleManager* InBattle,
+	const FPresentationRecordWriter& InPresentationRecordWriter
+)
+	: FTriggerContext(
+		FTriggerRuntimeSource::FromRelic(InRuntimeSource),
+		InActionOuter,
+		InBattle,
+		InPresentationRecordWriter
+	)
+{
+}
+
+FTriggerContext::FTriggerContext(
+	const FTriggerRuntimeSource& InRuntimeSource,
+	UObject* InActionOuter,
+	ABattleManager* InBattle,
+	const FPresentationRecordWriter& InPresentationRecordWriter
+)
 	: RuntimeSource(InRuntimeSource)
-	, Owner(IsValid(InRuntimeSource) ? InRuntimeSource->GetOwner() : nullptr)
 	, ActionOuter(InActionOuter)
 	, Battle(InBattle)
 	, PresentationRecordWriter(InPresentationRecordWriter)
 {
 }
 
+UObject* FTriggerContext::GetRuntimeSourceObject() const
+{
+	return RuntimeSource.RuntimeObject;
+}
+
 UStatusInstance* FTriggerContext::GetRuntimeSource() const
 {
-	return RuntimeSource;
+	return RuntimeSource.Kind == ETriggerRuntimeSourceKind::Status
+		? Cast<UStatusInstance>(RuntimeSource.RuntimeObject)
+		: nullptr;
+}
+
+URelicInstance* FTriggerContext::GetRelicSource() const
+{
+	return RuntimeSource.Kind == ETriggerRuntimeSourceKind::Relic
+		? Cast<URelicInstance>(RuntimeSource.RuntimeObject)
+		: nullptr;
+}
+
+ETriggerRuntimeSourceKind FTriggerContext::GetSourceKind() const
+{
+	return RuntimeSource.Kind;
+}
+
+FName FTriggerContext::GetSourceId() const
+{
+	return RuntimeSource.SourceId;
+}
+
+uint64 FTriggerContext::GetRuntimeSequence() const
+{
+	return RuntimeSource.RuntimeSequence;
 }
 
 ACombatant* FTriggerContext::GetOwner() const
 {
-	return Owner;
+	return RuntimeSource.CombatantOwner;
 }
 
 UObject* FTriggerContext::GetActionOuter() const
