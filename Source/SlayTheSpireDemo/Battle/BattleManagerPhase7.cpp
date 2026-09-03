317103da02824b696886a68ebe589f5193467b10
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-09-03 13:31:42 +0800

    Delegate BattleManager EnergyChanged records to shared helper

--- Source/SlayTheSpireDemo/Battle/BattleManagerPhase7.cpp
+++ Source/SlayTheSpireDemo/Battle/BattleManagerPhase7.cpp
@@ -1,8 +1,16 @@
 #include "BattleManager.h"
 
 #include "../Deck/DeckRuntime.h"
+#include "../Presentation/EnergyPresentationRecord.h"
 
 bool ABattleManager::IsAuthoritativeDeckRuntime(const UDeckRuntime* Deck) const
 {
 	return IsValid(DeckRuntime.Get()) && DeckRuntime.Get() == Deck;
+}
+
+void ABattleManager::AppendEnergyChangedPresentationRecord(
+	const FEnergyCommitResult& CommitResult,
+	const FPresentationRecordWriter& Writer)
+{
+	EnergyPresentationRecord::AppendCommittedEnergyChanged(CommitResult, Writer);
 }
