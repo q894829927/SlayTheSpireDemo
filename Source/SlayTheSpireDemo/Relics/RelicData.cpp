5aad439b61aad4689aa053c20d3d05a1267d973b
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-09-03 14:48:53 +0800

    Validate relic counter presentation metadata

--- Source/SlayTheSpireDemo/Relics/RelicData.cpp
+++ Source/SlayTheSpireDemo/Relics/RelicData.cpp
@@ -6,14 +6,22 @@
 EDataValidationResult URelicData::IsDataValid(FDataValidationContext& Context) const
 {
 	const EDataValidationResult ParentResult = Super::IsDataValid(Context);
+	bool bValid = ParentResult != EDataValidationResult::Invalid;
+
 	if (RelicId.IsNone())
 	{
 		Context.AddError(FText::FromString(TEXT("RelicId must not be None.")));
-		return EDataValidationResult::Invalid;
+		bValid = false;
 	}
 
-	return ParentResult == EDataValidationResult::Invalid
-		? EDataValidationResult::Invalid
-		: EDataValidationResult::Valid;
+	if (bShowCounter && CounterDisplayMax <= 0)
+	{
+		Context.AddError(FText::FromString(TEXT("CounterDisplayMax must be greater than zero when bShowCounter is enabled.")));
+		bValid = false;
+	}
+
+	return bValid
+		? EDataValidationResult::Valid
+		: EDataValidationResult::Invalid;
 }
 #endif
