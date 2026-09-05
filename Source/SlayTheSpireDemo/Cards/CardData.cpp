38f94a5da071ec71a2c118397c3f341fbd3f937f
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-09-05 15:53:03 +0800

    cfv1: validate authored card rarity and color domains

--- Source/SlayTheSpireDemo/Cards/CardData.cpp
+++ Source/SlayTheSpireDemo/Cards/CardData.cpp
@@ -4,11 +4,56 @@
 #include "Misc/DataValidation.h"
 #include "../Battle/BattleTextResolver.h"
 
+namespace
+{
+	bool IsAuthoredCardRarityValid(ECardRarity Rarity)
+	{
+		switch (Rarity)
+		{
+		case ECardRarity::Basic:
+		case ECardRarity::Common:
+		case ECardRarity::Uncommon:
+		case ECardRarity::Rare:
+		case ECardRarity::Special:
+		case ECardRarity::Curse:
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	bool IsAuthoredCardColorValid(ECardColor CardColor)
+	{
+		switch (CardColor)
+		{
+		case ECardColor::Red:
+		case ECardColor::Green:
+		case ECardColor::Blue:
+		case ECardColor::Purple:
+		case ECardColor::Colorless:
+		case ECardColor::Curse:
+			return true;
+		default:
+			return false;
+		}
+	}
+}
+
 EDataValidationResult UCardData::IsDataValid(FDataValidationContext& Context) const
 {
 	const EDataValidationResult ParentResult = Super::IsDataValid(Context);
 	bool bIsValid = ParentResult != EDataValidationResult::Invalid;
 
+	if (!IsAuthoredCardRarityValid(Rarity))
+	{
+		Context.AddError(FText::FromString(TEXT("Card Rarity contains an invalid enum value.")));
+		bIsValid = false;
+	}
+	if (!IsAuthoredCardColorValid(CardColor))
+	{
+		Context.AddError(FText::FromString(TEXT("Card CardColor contains an invalid enum value.")));
+		bIsValid = false;
+	}
 	if (BaseCost < 0)
 	{
 		Context.AddError(FText::FromString(TEXT("Card BaseCost cannot be negative.")));
