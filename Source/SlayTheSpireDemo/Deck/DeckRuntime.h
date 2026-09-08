9acb1da69aada0f50d08904ffe1cd61bf3257061
RUOXIN ZHANG <57868478+q894829927@users.noreply.github.com>
2026-09-08 16:29:29 +0900

    feat(wave1cc1): add hand-to-draw-top deck commit

--- Source/SlayTheSpireDemo/Deck/DeckRuntime.h
+++ Source/SlayTheSpireDemo/Deck/DeckRuntime.h
@@ -47,6 +47,7 @@ public:
 	FCardZoneMutationResult TryDrawTopCardCommit(UCardInstance*& OutCard);
 	FCardZoneMutationResult TryDiscardCardCommit(UCardInstance* Card);
 	FCardZoneMutationResult TryExhaustHandCardCommit(UCardInstance* Card);
+	FCardZoneMutationResult TryMoveHandCardToDrawPileTopCommit(UCardInstance* Card);
 	FCardZoneMutationResult TryMoveHandCardToPlayAreaCommit(UCardInstance* Card);
 	FCardZoneMutationResult TryReturnPlayAreaCardToHandAtIndexCommit(UCardInstance* Card, int32 HandIndex);
 	FCardZoneMutationResult TryMovePlayAreaCardToDestinationCommit(UCardInstance* Card, ECardDestination Destination);
